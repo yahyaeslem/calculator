a5a5b61e94096e127a16cfeb7683303d48387f00
yahyaeslem <yahyaeslem@gmail.com>
2022-12-31 20:16:35 +0300

    Created calculator , round handled, extra credits not created,

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
 function add (x,y){
-    return x + y;
+
+    return parseInt(x) + parseInt(y);
 }
 function subtract(x,y){
     return x-y;
@@ -8,9 +10,12 @@ function multiply(x,y){
     return x * y;
 }
 function divide(x,y){
+    
+   
     return x / y;
-
+    
 }
+//OPERATE FUNCTION
 function operate(operator, numOne, numTwo){
     if(operator === `+` ){
         result = add(numOne,numTwo)
@@ -19,22 +24,30 @@ function operate(operator, numOne, numTwo){
         result = subtract(numOne,numTwo)
     }
     else if(operator === `*` ){
-       result =  multiply (numOne,numTwo)
+        result =  multiply(numOne,numTwo)
     }
     else if(operator === `/` ){
+        if(numTwo == 0){
+            result = "Math Error"
+        }
+        else{
         result = divide(numOne,numTwo)
-
+        }
     }
- 
+result = (Math.round( result * 100 + Number.EPSILON ) / 100);
+ return result;
 }
-
+// VARIABLES
 const screen = document.querySelector(".screen");
 let screenVar ="";
-let leftVars;
+let leftVars ;
 let rightVars;
-let result;
+let result = 0;
 let operator;
+let operator2;
+let operatorCheck= false;
 screen.textContent = screenVar;
+//DOM ELEMENTS
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
 const btn3 = document.querySelector(".btn3");
@@ -51,20 +64,82 @@ const btnMinus = document.querySelector(".btnMinus");
 const btnMultiply = document.querySelector(".btnMultiply");
 const btnDivide = document.querySelector(".btnDivide");
 const btnEquals = document.querySelector(".btnEquals");
-btn1.addEventListener('click', ()=> {screenVar += "1" ; screen.textContent = screenVar})
-btn2.addEventListener('click', ()=> {screenVar += "2" ; screen.textContent = screenVar})
-btn3.addEventListener('click', ()=> {screenVar += "3" ; screen.textContent = screenVar})
-btn4.addEventListener('click', ()=> {screenVar += "4" ; screen.textContent = screenVar})
-btn5.addEventListener('click', ()=> {screenVar += "5" ; screen.textContent = screenVar})
-btn6.addEventListener('click', ()=> {screenVar += "6" ; screen.textContent = screenVar})
-btn7.addEventListener('click', ()=> {screenVar += "7" ; screen.textContent = screenVar})
-btn8.addEventListener('click', ()=> {screenVar += "8" ; screen.textContent = screenVar})
-btn9.addEventListener('click', ()=> {screenVar += "9" ; screen.textContent = screenVar})
-btn0.addEventListener('click', ()=> {screenVar += "0" ; screen.textContent = screenVar})
-btnClear.addEventListener('click', ()=> {screenVar = "" ; screen.textContent = screenVar})
-btnPlus.addEventListener('click', ()=> { operator = "+";leftVars= parseInt(screenVar); screenVar+= "+"; screen.textContent =screenVar;})
-btnMinus.addEventListener('click', ()=> {operator = "-";leftVars =parseInt(screenVar); screenVar+= "-" ; screen.textContent = screenVar ; })
+//NUMBER BUTTONS
+btn1.addEventListener('click', ()=> {screenVar += "1" ; screen.textContent = screenVar ; rightVars= screenVar.substring(screenVar.indexOf(operator) +1 );})
+btn2.addEventListener('click', ()=> {screenVar += "2" ; screen.textContent = screenVar ; rightVars= screenVar.substring(screenVar.indexOf(operator) +1 );})
+btn3.addEventListener('click', ()=> {screenVar += "3" ; screen.textContent = screenVar ; rightVars= screenVar.substring(screenVar.indexOf(operator) +1 );})
+btn4.addEventListener('click', ()=> {screenVar += "4" ; screen.textContent = screenVar ; rightVars= screenVar.substring(screenVar.indexOf(operator) +1 );})
+btn5.addEventListener('click', ()=> {screenVar += "5" ; screen.textContent = screenVar ; rightVars= screenVar.substring(screenVar.indexOf(operator) +1 );})
+btn6.addEventListener('click', ()=> {screenVar += "6" ; screen.textContent = screenVar ; rightVars= screenVar.substring(screenVar.indexOf(operator) +1 );})
+btn7.addEventListener('click', ()=> {screenVar += "7" ; screen.textContent = screenVar ; rightVars= screenVar.substring(screenVar.indexOf(operator) +1 );})
+btn8.addEventListener('click', ()=> {screenVar += "8" ; screen.textContent = screenVar ; rightVars= screenVar.substring(screenVar.indexOf(operator) +1 );})
+btn9.addEventListener('click', ()=> {screenVar += "9" ; screen.textContent = screenVar ; rightVars= screenVar.substring(screenVar.indexOf(operator) +1 );})
+btn0.addEventListener('click', ()=> {screenVar += "0" ; screen.textContent = screenVar ; rightVars= screenVar.substring(screenVar.indexOf(operator) +1 );})
+btnClear.addEventListener('click', ()=> {screenVar = "" ; screen.textContent = screenVar ; screenVar ="";
+ leftVars = ""
+ rightVars =""
+ result = 0;
+ operator2="";
+operator="";
+ operatorCheck= false;})
+//PLUS  BUTTON
+btnPlus.addEventListener('click', 
+()=> {
+    if(operatorCheck){
+        operator2 = "+"; evaluate()}
+        else{
+            operator = "+"; operatorCheck=true; 
+            leftVars =screenVar.substring( (0, screenVar.indexOf(operator))); screenVar+= "+"; 
+            rightVars= screenVar.substring(screenVar.indexOf(operator) +1 ); 
+            screen.textContent =screenVar;}})
+// MINUS BUTTON
+btnMinus.addEventListener('click', 
+()=> {
+    if(operatorCheck){
+        operator2 = "-";  evaluate()}
+        else{
+    operator = "-"; operatorCheck=true;  
+    leftVars =screenVar.substring( (0, screenVar.indexOf(operator)));   screenVar+= "-" ; 
+    rightVars= screenVar.substring(screenVar.indexOf(operator) +1 ); 
+    screen.textContent = screenVar; }})
+// MULTIPLY BUTTON
+btnMultiply.addEventListener('click', 
+()=> { 
+    if(operatorCheck){
+        operator2 = "*";  evaluate()}
+        else{
+    operator = "*"; operatorCheck=true; 
+    leftVars =screenVar.substring( (0, screenVar.indexOf(operator))); screenVar+= "*";
+    rightVars= screenVar.substring(screenVar.indexOf(operator) +1 ); 
+    screen.textContent = screenVar ;}})
+//DIVIDE BUTTON
+btnDivide.addEventListener('click', 
+()=> { 
+    if(operatorCheck){
+        operator2 = "/" ; evaluate()}
+        else{
+    operator = "/" ; operatorCheck=true; 
+    leftVars =screenVar.substring( (0, screenVar.indexOf(operator))); screenVar+= "/"; 
+    rightVars= screenVar.substring(screenVar.indexOf(operator) +1 ); 
+    screen.textContent = screenVar ;}})
+//EQUALS BUTTON
+btnEquals.addEventListener('click', 
+()=> {rightVars = screenVar.substring(screenVar.indexOf(operator) +1 ) 
+        operate(operator,leftVars,rightVars); screenVar=""; 
+        screen.textContent= result})
 
-btnMultiply.addEventListener('click', ()=> {operator = "*";leftVars =parseInt(screenVar); screenVar+= "*"; screen.textContent = screenVar ;})
-btnDivide.addEventListener('click', ()=> { operator = "/" ;leftVars =parseInt(screenVar); screenVar+= "/"; screen.textContent = screenVar ;})
-btnEquals.addEventListener('click', ()=> {rightVars= screenVar.slice(screenVar.indexOf(operator) + 1); operate(operator,leftVars,rightVars); screenVar=""; screen.textContent= result})
+function evaluate(){
+     
+    
+   operate(operator,leftVars,rightVars);
+   leftVars=result.toString();
+   rightVars="0";
+    screenVar= leftVars + operator2;
+   
+     operator= operator2;
+   screen.textContent = leftVars ;
+     
+   
+   
+
+}
